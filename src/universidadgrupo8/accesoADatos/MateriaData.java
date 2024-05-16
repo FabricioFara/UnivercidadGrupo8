249a1d2aa7c01e592654c1ab76eea013f7ac5d35
FabricioFara <farafabri1994@gmail.com>
2024-05-16 17:25:16 -0300

    d

--- src/universidadgrupo8/accesoADatos/MateriaData.java
+++ src/universidadgrupo8/accesoADatos/MateriaData.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package universidadgrupo8.accesoADatos;
 
-/**
- *
- * @author Ryzen 5 5600G
- */
+import java.sql.Connection;
+
+
 public class MateriaData {
+        
+     Connection con = null;
+
+    public MateriaData() {
+        
+        con = Conexion.getConexion();
+    }
+     
+     
+    
     
 }
