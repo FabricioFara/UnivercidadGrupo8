8e64644d288a02bf8f0571320a071b7838235bae
FabricioFara <farafabri1994@gmail.com>
2024-05-21 11:32:07 -0300

    corregido

--- src/universidadgrupo8/accesoADatos/InscripcionData.java
+++ src/universidadgrupo8/accesoADatos/InscripcionData.java
@@ -2,6 +2,8 @@
 package universidadgrupo8.accesoADatos;
 
 import java.sql.*;
+import javax.swing.JOptionPane;
+import universidadgrupo8.entidad.Inscripcion;
 
 
 public class InscripcionData {
@@ -13,5 +15,29 @@ public class InscripcionData {
         this.con = Conexion.getConexion();
     }
     
+    public void guardarInscripcion(Inscripcion insc){
     
+        String sql = "INSERT INTO inscripciones (id_alumnos,id_materia,nota) VALUES (?,?,?) ";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, insc.getAlumno().getIdAlumno());
+            ps.setInt(2, insc.getMateria().getIdMateria());
+            ps.setDouble(3, insc.getNota());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            
+            if(rs.next()){
+                insc.setIdInscripcion(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Inscripcion registrada");
+            
+            }
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "error a acceder a la tabla de inscripciones");
+        }
+    
+    
+    }
 }
