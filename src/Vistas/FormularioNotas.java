df0e10ddb1ca9beba988c51ba9c4af0c022305af
Rocio-anabel <163307652+Rocio-anabel@users.noreply.github.com>
2024-05-28 09:54:21 -0300

    Update Formulario Notas
    Eventos y métodos agregados

--- src/Vistas/FormularioNotas.java
+++ src/Vistas/FormularioNotas.java
@@ -5,7 +5,13 @@
  */
 package Vistas;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import universidadgrupo8.accesoADatos.AlumnoData;
+import universidadgrupo8.accesoADatos.InscripcionData;
 import universidadgrupo8.entidad.Alumno;
+import universidadgrupo8.entidad.Inscripcion;
 
 /**
  *
@@ -13,11 +19,22 @@ import universidadgrupo8.entidad.Alumno;
  */
 public class FormularioNotas extends javax.swing.JInternalFrame {
 
+    private ArrayList<Alumno> listaA;
+    private AlumnoData aData;
+    private InscripcionData inscData;
+    private ArrayList<Inscripcion> listaI;
+    private DefaultTableModel modelo;
     /**
      * Creates new form FormularioNotas
      */
     public FormularioNotas() {
         initComponents();
+        aData = new AlumnoData();
+        inscData = new InscripcionData();
+        listaA = (ArrayList<Alumno>) aData.listarAlumnos();
+        modelo = new DefaultTableModel();
+        cargarAlumnos();
+        armarCabeceraTabla();
     }
 
     /**
@@ -46,7 +63,11 @@ public class FormularioNotas extends javax.swing.JInternalFrame {
         jLabel2.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jLabel2.setText("Seleccione un alumno:");
 
-        jcAlumnos.setSelectedIndex(-1);
+        jcAlumnos.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                jcAlumnosItemStateChanged(evt);
+            }
+        });
         jcAlumnos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jcAlumnosActionPerformed(evt);
@@ -74,6 +95,11 @@ public class FormularioNotas extends javax.swing.JInternalFrame {
         });
 
         jbSalir.setText("Salir");
+        jbSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbSalirActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -130,12 +156,29 @@ public class FormularioNotas extends javax.swing.JInternalFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jbGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbGuardarActionPerformed
-        // TODO add your handling code here:
+        int filaSeleccionada = jTabla.getSelectedRow();
+        if (filaSeleccionada != -1){
+            Alumno a = (Alumno)jcAlumnos.getSelectedItem();
+            int idAlumno = a.getIdAlumno();
+            int idMateria = (Integer)modelo.getValueAt(filaSeleccionada, 0);
+            double nota = Double.parseDouble((String)modelo.getValueAt(filaSeleccionada, 2));
+            inscData.actualizarNota(idAlumno, idMateria, nota);
+        } else {
+            JOptionPane.showMessageDialog(this, "No ha seleccionado una fila");
+        }
     }//GEN-LAST:event_jbGuardarActionPerformed
 
     private void jcAlumnosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jcAlumnosActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jcAlumnosActionPerformed
+
+    private void jcAlumnosItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_jcAlumnosItemStateChanged
+        cargaNotas();
+    }//GEN-LAST:event_jcAlumnosItemStateChanged
+
+    private void jbSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbSalirActionPerformed
+        dispose();
+    }//GEN-LAST:event_jbSalirActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -148,4 +191,45 @@ public class FormularioNotas extends javax.swing.JInternalFrame {
     private javax.swing.JButton jbSalir;
     private javax.swing.JComboBox<Alumno> jcAlumnos;
     // End of variables declaration//GEN-END:variables
+
+    private void cargarAlumnos() {
+        for (Alumno item : listaA) {
+            jcAlumnos.addItem(item);
+        }
+    }
+
+    private void armarCabeceraTabla() {
+         ArrayList<Object> filaCabecera = new ArrayList<>();
+         filaCabecera.add("Código");
+         filaCabecera.add("Nombre");
+         filaCabecera.add("Nota");
+         for (Object it : filaCabecera) {
+            modelo.addColumn(it);
+         }
+         jTabla.setModel(modelo);
+                 
+    }
+    
+    private void borrarFilaTabla(){
+        int indice = modelo.getRowCount() - 1;
+        
+        for (int i = indice; i >= 0; i--) {
+            modelo.removeRow(i);
+        }
+    }
+    
+    private void cargaNotas(){
+        borrarFilaTabla();
+        Alumno selec = (Alumno)jcAlumnos.getSelectedItem();
+        listaI = (ArrayList<Inscripcion>) inscData.obtenerInscripcionesPorAlumno(selec.getIdAlumno());
+        
+        for (Inscripcion insc : listaI) {
+            int idMateria = insc.getMateria().getIdMateria();
+            String nombre = insc.getMateria().getNombre();
+            double nota = insc.getNota();
+            modelo.addRow(new Object[] {idMateria, nombre, nota});
+        }
+            
+        
+    }
 }
